9ed8fabc42d008802a411badec40ae1366f301c4
Prabu <prabu.manickam@directlinegroup.co.uk>
2026-04-30 17:58:39 +0100

    form-sdk migrated to form-angular-sdk

--- forms-angular-new/src/app/episerver-forms/index.ts
+++ forms-angular-new/src/app/episerver-forms/index.ts
@@ -6,14 +6,35 @@ export * from './components/form-container-block/form-container-block.component'
 export * from './components/form-body/form-body.component';
 export * from './components/render-element/render-element.component';
 export * from './components/render-element-in-step/render-element-in-step.component';
+export * from './components/forms/dynamic-field/dynamic-field.component';
 export * from './components/forms/form-container/form-container.component';
 export * from './components/forms/form-step-navigation/form-step-navigation.component';
+export * from './components/forms/validation-message/validation-message.component';
 export * from './components/forms/data-list/data-list.component';
 export * from './components/forms/element-caption/element-caption.component';
 export * from './components/forms/element-wrapper/element-wrapper.component';
+export * from './components/forms/fields/choice-field/choice-field.component';
+export * from './components/forms/fields/file-upload-field/file-upload-field.component';
+export * from './components/forms/fields/form-step-field/form-step-field.component';
+export * from './components/forms/fields/hidden-field/hidden-field.component';
+export * from './components/forms/fields/image-choice-field/image-choice-field.component';
+export * from './components/forms/fields/number-field/number-field.component';
+export * from './components/forms/fields/paragraph-field/paragraph-field.component';
+export * from './components/forms/fields/range-field/range-field.component';
+export * from './components/forms/fields/reset-button-field/reset-button-field.component';
+export * from './components/forms/fields/select-field/select-field.component';
+export * from './components/forms/fields/submit-button-field/submit-button-field.component';
+export * from './components/forms/fields/text-field/text-field.component';
+export * from './components/forms/fields/textarea-field/textarea-field.component';
+export * from './components/forms/fields/url-field/url-field.component';
+export { SelectFieldComponent as ControlSelectFieldComponent, type SelectFieldOption } from './components/custom-controls/select-field/select-field.component';
+export { TextFieldComponent as ControlTextFieldComponent } from './components/custom-controls/text-field/text-field.component';
+export { TextareaFieldComponent as ControlTextareaFieldComponent } from './components/custom-controls/textarea-field/textarea-field.component';
 
+export * from './models/dynamic-episerver-form.model';
 export * from './models/form-schema.model';
 
+export * from './services/dynamic-form-adapter.service';
 export * from './services/form-auth.service';
 export * from './services/form-confirmation.service';
 export * from './services/form-loader.service';
